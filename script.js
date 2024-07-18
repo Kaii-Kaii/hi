44fb95baecdc6b70b2b6944da3a438287a57f9a6
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-07-18 22:13:36 +0700

    fix

--- script.js
+++ script.js
@@ -1,197 +1,99 @@
-class PointerParticle {
-  constructor(spread, speed, component) {
-      const { ctx, pointer, hue } = component;
+const POT_BOTTOM_Y = 585;
+const POTS_TIMES = [
+{
+  duration: 0.5 + Math.random() * 0.3,
+  delay: 0 },
 
-      this.ctx = ctx;
-      this.x = pointer.x;
-      this.y = pointer.y;
-      this.mx = pointer.mx * 0.1;
-      this.my = pointer.my * 0.1;
-      this.size = Math.random() + 1;
-      this.decay = 0.01;
-      this.speed = speed * 0.08;
-      this.spread = spread * this.speed;
-      this.spreadX = (Math.random() - 0.5) * this.spread - this.mx;
-      this.spreadY = (Math.random() - 0.5) * this.spread - this.my;
-      this.color = `hsl(${hue}deg 90% 60%)`;
+{
+  duration: 0.5 + Math.random() * 0.3,
+  delay: 0.5 },
+
+{
+  duration: 0.5 + Math.random() * 0.3,
+  delay: 0.2 }];
+
+
+const masterTimeline = new TimelineMax();
+
+const potsTimeline = [...document.querySelectorAll('.pot')].map((pot, i) => {
+  const timeline = new TimelineMax();
+
+  timeline.from(
+  pot,
+  POTS_TIMES[i].duration,
+  {
+    delay: POTS_TIMES[i].delay,
+    y: -POT_BOTTOM_Y });
+
+
+
+  return timeline;
+});
+const shadowsTimeline = [...document.querySelectorAll('.pot-shadow')].map((shadow, i) => {
+  const timeline = new TimelineMax();
+
+  timeline.from(
+  shadow,
+  POTS_TIMES[i].duration,
+  {
+    ease: Power0.easeInOut,
+    delay: POTS_TIMES[i].delay,
+    scale: 0,
+    transformOrigin: 'center center' });
+
+
+
+  return timeline;
+});
+const leafsTimeline = [...document.querySelectorAll('.leaf')].map(leaf => {
+  const isBack = leaf.classList.contains('leaf-back');
+
+  const timeline = new TimelineMax();
+
+  timeline.from(
+  leaf,
+  0.5 + Math.random() * 0.8,
+  {
+    delay: isBack ? 0.5 : 0,
+    ease: Back.easeOut.config(0.5 + Math.random()),
+    scale: 0,
+    yPercent: 10 * Math.random(),
+    transformOrigin: 'center bottom' });
+
+
+
+  return timeline;
+});
+const flowersTimeline = [...document.querySelectorAll('.flower')].map(flower => {
+  const isLeft = flower.classList.contains('flower-left');
+
+  const timeline = new TimelineMax();
+
+  timeline.from(
+  flower,
+  1 + Math.random() * 1,
+  {
+    ease: Circ.easeOut,
+    scale: 0,
+    transformOrigin: isLeft ? 'right bottom' : 'left bottom' });
+
+
+
+  return timeline;
+});
+
+masterTimeline.
+to('.replay-text', 0, { opacity: 0 }).
+add(potsTimeline, 0).
+add(shadowsTimeline, 0).
+add(leafsTimeline).
+add(flowersTimeline).
+to('.replay-text', .5, { opacity: 1 });
+
+
+
+document.body.addEventListener('click', () => {
+  if (!masterTimeline.isActive()) {
+    masterTimeline.restart();
   }
-
-  draw() {
-      this.ctx.fillStyle = this.color;
-      this.ctx.beginPath();
-      this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-      this.ctx.fill();
-  }
-
-  collapse() {
-      this.size -= this.decay;
-  }
-
-  trail() {
-      this.x += this.spreadX * this.size;
-      this.y += this.spreadY * this.size;
-  }
-
-  update() {
-      this.draw();
-      this.trail();
-      this.collapse();
-  }
-}
-
-class PointerParticles extends HTMLElement {
-  static register(tag = "pointer-particles") {
-      if ("customElements" in window) {
-          customElements.define(tag, this);
-      }
-  }
-
-  static css = `
-    :host {
-      display: grid;
-      width: 100%;
-      height: 100%;
-      pointer-events: none;
-    }
-  `;
-
-  constructor() {
-      super();
-
-      this.canvas;
-      this.ctx;
-      this.fps = 60;
-      this.msPerFrame = 1000 / this.fps;
-      this.timePrevious;
-      this.particles = [];
-      this.pointer = {
-          x: 0,
-          y: 0,
-          mx: 0,
-          my: 0
-      };
-      this.hue = 0;
-  }
-
-  connectedCallback() {
-      const canvas = document.createElement("canvas");
-      const sheet = new CSSStyleSheet();
-
-      this.shadowroot = this.attachShadow({ mode: "open" });
-
-      sheet.replaceSync(PointerParticles.css);
-      this.shadowroot.adoptedStyleSheets = [sheet];
-
-      this.shadowroot.append(canvas);
-
-      this.canvas = this.shadowroot.querySelector("canvas");
-      this.ctx = this.canvas.getContext("2d");
-      this.setCanvasDimensions();
-      this.setupEvents();
-      this.timePrevious = performance.now();
-      this.animateParticles();
-  }
-
-  createParticles(event, { count, speed, spread }) {
-      this.setPointerValues(event);
-
-      for (let i = 0; i < count; i++) {
-          this.particles.push(new PointerParticle(spread, speed, this));
-      }
-  }
-
-  setPointerValues(event) {
-      const isTouchEvent = event.type.startsWith('touch');
-      const point = isTouchEvent ? event.touches[0] : event;
-
-      this.pointer.x = point.clientX - this.offsetLeft;
-      this.pointer.y = point.clientY - this.offsetTop;
-      this.pointer.mx = isTouchEvent ? 0 : event.movementX;
-      this.pointer.my = isTouchEvent ? 0 : event.movementY;
-  }
-
-  setupEvents() {
-      const parent = this.parentNode;
-
-      parent.addEventListener("click", (event) => {
-          this.createParticles(event, {
-              count: 300,
-              speed: Math.random() + 1,
-              spread: Math.random() + 50
-          });
-      });
-
-      parent.addEventListener("pointermove", (event) => {
-          this.createParticles(event, {
-              count: 20,
-              speed: this.getPointerVelocity(event),
-              spread: 1
-          });
-      });
-
-      // Add touch events
-      parent.addEventListener("touchstart", (event) => {
-          this.createParticles(event, {
-              count: 300,
-              speed: Math.random() + 1,
-              spread: Math.random() + 50
-          });
-      });
-
-      parent.addEventListener("touchmove", (event) => {
-          this.createParticles(event, {
-              count: 20,
-              speed: this.getPointerVelocity(event),
-              spread: 1
-          });
-      });
-
-      window.addEventListener("resize", () => this.setCanvasDimensions());
-  }
-
-  getPointerVelocity(event) {
-      const a = event.movementX;
-      const b = event.movementY;
-      const c = Math.floor(Math.sqrt(a * a + b * b));
-
-      return c;
-  }
-
-  handleParticles() {
-      for (let i = 0; i < this.particles.length; i++) {
-          this.particles[i].update();
-
-          if (this.particles[i].size <= 0.1) {
-              this.particles.splice(i, 1);
-              i--;
-          }
-      }
-  }
-
-  setCanvasDimensions() {
-      const rect = this.parentNode.getBoundingClientRect();
-
-      this.canvas.width = rect.width;
-      this.canvas.height = rect.height;
-  }
-
-  animateParticles() {
-      requestAnimationFrame(() => this.animateParticles());
-
-      const timeNow = performance.now();
-      const timePassed = timeNow - this.timePrevious;
-
-      if (timePassed < this.msPerFrame) return;
-
-      const excessTime = timePassed % this.msPerFrame;
-
-      this.timePrevious = timeNow - excessTime;
-
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.hue = this.hue > 360 ? 0 : (this.hue += 3);
-
-      this.handleParticles();
-  }
-}
-
-PointerParticles.register();
+});
